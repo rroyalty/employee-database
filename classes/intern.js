6311a500d28a34eb9a2871b5cd727525dff81f1a
rroyalty <rroyalty@gmail.com>
2021-02-28 11:18:50 -0500

    Almost done!

--- classes/intern.js
+++ classes/intern.js
@@ -1,10 +1,13 @@
 const Employee = require('./employee');
 
 class Intern extends Employee {
-    constructor(school, role, name, id, email) {
-        super(role, name, id, email);
+    constructor(school, name, id, email) {
+        super(name, id, email);
         this.school = school;
     }
+
+    getSchool() { return this.school};
+    getClass() {return this.constructor.name}
 }
 
 module.exports = Intern;
